a439978a10c487e66eff57cab23495f23aec5d79
boaten01@gmail.com <boaten01@gmail.com@5a5fe92c-118f-c754-34de-499c13ac2675>
2014-03-14 11:44:08 +0000

    Added assets. Added TextPacker2 to process and create texture maps

---
+++ trunk/dantesbarbarque-desktop/src/net/asg/games/dante/Main.java
@@ -0,0 +1,28 @@
+package net.asg.games.dante;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
+
+public class Main {
+	public static void main(String[] args) {
+		boolean copyImages = false;	
+		
+		if (copyImages) {
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			TexturePacker2.process(settings, "images", "../dantesbarbarque-android/assets", "game");
+			TexturePacker2.process(settings, "text-images", "../dantesbarbarque-android/assets", "text_images");
+		}
+		
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "Dante's Barbarque";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new DantesBarbarqueGame(), cfg);
+	}
+}
