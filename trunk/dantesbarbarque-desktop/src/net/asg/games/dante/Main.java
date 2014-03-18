f51f1e67b1fce4b52333989788c42e5a40e26593
boaten01@gmail.com <boaten01@gmail.com@5a5fe92c-118f-c754-34de-499c13ac2675>
2014-03-18 11:48:16 +0000

    Added Wall.java Added new assets to project Created Block.java for world rendering

--- trunk/dantesbarbarque-desktop/src/net/asg/games/dante/Main.java
+++ trunk/dantesbarbarque-desktop/src/net/asg/games/dante/Main.java
@@ -20,8 +20,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Dante's Barbarque";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 800;
+		cfg.height = 480;
 		
 		new LwjglApplication(new DantesBarbarqueGame(), cfg);
 	}
